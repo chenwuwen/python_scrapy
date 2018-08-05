f7830aacce228b9ebd7a7572cabd796840a14441
kanyun <949955482@qq.com>
2018-08-05 18:41:17 +0800

    Initial commit

---
+++ python_scrapy/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# 爬虫item， 用于定义数据结构
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PythonScrapyItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
---
+++ python_scrapy/pipelines.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 爬虫pipeline，用于处理提取的结构，比如清洗数据、去重等
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class PythonScrapyPipeline(object):
+    def process_item(self, item, spider):
+        return item
